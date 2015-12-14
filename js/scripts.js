1f35103c0bb25ccceff49bfc8414c6ec737d732c
Carl Humphreys <carlh.crimson@gmail.com>
2015-12-14 11:37:49 +0000

    init and resizing
    best viewed landscape on phones, still needs some tweaks for correct
    ratios/image sizes

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,64 @@
 var canvas = document.querySelector("#clippingCanvas");
 var ctx = canvas.getContext("2d");
+var container = document.querySelector("#canvasContainer");
+var canvasWidth, canvasHeight;
 
-var stars = [];
-
-
-
-// set the clipping shape
-
+// set the clipping image
 var img = new Image();
 img.src = "https://i.guim.co.uk/img/static/sys-images/Guar" +
 "dian/Pix/pictures/2011/12/13/1323770749407/Decorated-Christmas-tree--007.jpg"+
 "?w=620&q=85&auto=format&sharp=10&s=05136b277a0cf0e620a5ec1ff1b44044";
 
 
-var drawCircle = function(x,y,radius) {
 
-	ctx.beginPath();
-	ctx.arc( x, y, radius, 0,2*Math.PI);	
-	ctx.fill();
+var particles = [];
+
+
+var init = function() {
+	console.log("initialising");
+
+	resize();
+
+	window.addEventListener("resize", function() {
+	resize();
+	});
+
+	windTap();
+
+		// # particles, maxX, maxY to start, radius, x speed, y speed
+		makeParticle(15, canvasWidth, canvasHeight, 15, 3, 2);
+         ctx.drawImage(img,0,0, canvasWidth, canvasHeight);
+         render();  
+}
+
+
+
+var resize = function() {
+	console.log("resizing");
+	canvasWidth = 0.9 * container.offsetWidth;
+	canvasHeight = 0.9 * container.offsetHeight;
+
+	canvas.width = canvasWidth;
+	canvas.height = canvasHeight;
+
 };
 
-var drawStar = function(x,y, radius, twinkle) {
+img.onload = function() {
+	console.log("image loaded");
+       init();
 
+ };
+
+
+var windTap = function() {
+
+};
+
+
+
+
+var drawShape = function(type, x,y,radius) {
+	
 	var theta = (360/5) * (Math.PI/180);
 	var correction = (Math.PI/180) * 180;
 	
@@ -32,73 +69,93 @@ var drawStar = function(x,y, radius, twinkle) {
 	ctx.translate( -x, -y );
 	/////
 
-	ctx.beginPath();
-	ctx.moveTo(x,y+radius);
-	ctx.lineTo(x + radius*Math.sin(2*theta), y + radius*Math.cos(2*theta));
-	ctx.lineTo(x + radius*Math.sin(4*theta), y + radius*Math.cos(4*theta));
-	ctx.lineTo(x + radius*Math.sin(1*theta), y + radius*Math.cos(1*theta));
-	ctx.lineTo(x + radius*Math.sin(3*theta), y + radius*Math.cos(3*theta));
-	ctx.lineTo(x,y+radius);
-	ctx.closePath();
-	ctx.fill();
+	if (type === "circle") {
+		ctx.beginPath();
+		ctx.arc( x, y, radius, 0,2*Math.PI);	
+		ctx.fill();
+	}
+	else if (type === "star") {
+
+		ctx.beginPath();
+		ctx.moveTo(x,y+radius);
+		ctx.lineTo(x + radius*Math.sin(2*theta), y + radius*Math.cos(2*theta));
+		ctx.lineTo(x + radius*Math.sin(4*theta), y + radius*Math.cos(4*theta));
+		ctx.lineTo(x + radius*Math.sin(1*theta), y + radius*Math.cos(1*theta));
+		ctx.lineTo(x + radius*Math.sin(3*theta), y + radius*Math.cos(3*theta));
+		ctx.lineTo(x,y+radius);
+		ctx.closePath();
+		ctx.fill();
+	}
+
 
 	// restore original orientation
 	ctx.restore();
-	///
+	/////
+	
+};
 
-}
 
- // draw cropped image when loaded
-img.onload = function() {
-       	makeStars(20, 500, 20, 20);
-         ctx.drawImage(img,0,0);
-         render();  
 
- };
-//make image to be clipped
 
-var makeStars = function(n, maxX, minY, maxRadius) {
+
+var makeParticle = function(n, maxX, minY, maxRadius, speedX, speedY) {
 	for (var i =0; i < n; i++) {
+		
+			var particle = new Particle();
+		 	particle.x = Math.round(((maxX - (2*maxRadius) - 1) * Math.random())+1);
+	 		particle.y = Math.random() * (-1* minY);
+	 		particle.radius = Math.round(((maxRadius-1) * Math.random())+1);
+	 		particle.speed.x = Math.floor(Math.random()*speedX) - (speedX/2);
+	 		particle.speed.y = 1 + (speedY * Math.random());
 
-		var star = new Star();
-	 	star.x = Math.round(((maxX - (2*maxRadius) - 1) * Math.random())+1);
- 		star.y = -2*maxRadius;
- 		star.radius = Math.round(((maxRadius-1) * Math.random())+1);
- 		star.speed.x = (1 + (-1 * Math.random() * 2));
- 		star.speed.y = 5 * Math.random();
-
-		stars.push(star);
+			particles.push(particle);
+		
 	}
 };
-var Star = function() {
+var Particle = function() {
 	this.x = 0;
 	this.y = 0;
 	this.radius = 0; 
 	this.speed = {x:0, y:0};
 }
 
+
 var y = 0;
 var render = function() {
 	requestAnimationFrame(render);
 
-	ctx.clearRect(0,0,500,500);
+	ctx.clearRect(0,0,canvasWidth,canvasHeight);
 	ctx.save();	
 	
 
-	for (var i = 0; i < stars.length; i++) {
-		var currentStar = stars[i];
+	for (var i = 0; i < particles.length; i++) {
+		var currentParticle = particles[i];
 
-		drawStar((currentStar.x += currentStar.speed.x), (currentStar.y += currentStar.speed.y), currentStar.radius);
+		drawShape("circle", (currentParticle.x += currentParticle.speed.x), (currentParticle.y += currentParticle.speed.y), currentParticle.radius);
 
-		if(currentStar.y - (2*currentStar.radius) > 500) {
-			currentStar.y = (-2*currentStar.radius);
+		// if(currentParticle.y - (2*currentParticle.radius) > 500) {
+		// 	currentParticle.y = (-2*currentParticle.radius);
+		// }
+		// if (currentParticle.x - (2*currentParticle.radius) > 500) {
+		// 	currentParticle.x = (-currentParticle.radius);
+		// }
+		// if (currentParticle.x  < 0) {
+		// 	currentParticle.x = (500 + currentParticle.radius);
+		// }
+
+		if (currentParticle.y >= canvasHeight + currentParticle.radius) {
+			
+		 	currentParticle.y = 0 - (2*currentParticle.radius);		 
+
 		}
-		if (currentStar.x - (2*currentStar.radius) > 500) {
-			currentStar.x = (-currentStar.radius);
-		}
-		if (currentStar.x  < 0) {
-			currentStar.x = (500 + currentStar.radius);
-		}
+ 		if (currentParticle.x >= canvasWidth + currentParticle.radius) {
+
+		 	currentParticle.x = 0 - currentParticle.radius;		
+
+ 		}
+ 		if (currentParticle.x < (0-currentParticle.radius)) {
+ 			currentParticle.x = canvasWidth + currentParticle.radius;
+ 		}
 	}
 
 
@@ -106,7 +163,7 @@ var render = function() {
 	// drawStar(y-200, y-200, 100);
 	
 	ctx.globalCompositeOperation="source-in";
-	ctx.drawImage(img,0,0, 500,500);
+	ctx.drawImage(img,0,0, canvasWidth, canvasHeight);
 	
 
 	ctx.restore();
